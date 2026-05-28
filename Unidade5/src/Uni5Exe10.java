3a136b9539a837c525bd190317b527923034707e
sophiavolani <spvolani@gmail.com>
2026-05-28 19:12:34 -0300

    commit dia 28/05

--- Unidade5/src/Uni5Exe10.java
+++ Unidade5/src/Uni5Exe10.java
@@ -1,5 +1,15 @@
 public class Uni5Exe10 {
     public static void main(String[] args) {
-        
+        for (int i = 0; i <= 10000; i++){
+            for(int j = 0; j <= 10000; j++){
+                System.out.println(i + "+" + j + "=" + (i+j));
+                int soma = j+ i;
+                String parte1 = i + "" + j;
+                String parte2 = "" + Math.pow(soma,2);
+                if (parte1.equals(parte2)){
+                    System.out.println(parte1 + "-" + parte2);
+                }
+            }
+        }
     }
 }
